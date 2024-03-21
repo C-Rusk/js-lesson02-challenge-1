ad3b80f1c42905918f9d02c9b3ec224755359320
Cassandra Rusk <149620042+C-Rusk@users.noreply.github.com>
2024-03-21 13:10:49 -0500

    JavaScript Challenge 1

---
+++ script.js
@@ -0,0 +1,6 @@
+var tempCelcius = 10;
+var toFahrenheit = tempCelcius * 1.8 + 32;
+
+console.log(
+  `Outside my friend's house it's ${tempCelcius} degrees Celcius, which is ${toFahrenheit} degrees Fahrenheit.`
+);
